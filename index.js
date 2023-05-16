758635d81738efac713429d59fcb096ea7da0e90
DhanushM12 <dhanushmukhi@gmail.com>
2023-05-16 17:52:04 +0530

    added verify

--- index.js
+++ index.js
@@ -1,11 +1,60 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const jwt = require('jsonwebtoken')
 
 app.get('/', (req, res) => {
     // res.send('<h1>Welcome to Node Js!!!</h1>')
     res.json({message: "Welcome to node"})
 })
+
+app.post('/tokenGenerate', (req, res) => {
+    const user = {
+        id: 1234,
+        username: 'Oct22',
+        email: 'Oct22@coding.com'
+    }
+    jwt.sign(user, 'secret', {expiresIn: '60s'}, function(err, token) {
+        if(err){
+            res.sendStatus(403)
+        }
+        else{
+            res.json({
+                token
+            })
+        }
+    });
+})
+
+app.post('/verifyToken', extractToken, (req, res) => {
+    jwt.verify(req.token, 'secret', function(err, data) {
+        if(err){
+            res.sendStatus(403)
+        }
+        else{
+            res.json({
+                message: 'User access granted', 
+                data
+            })
+        }
+    });
+})
+
+
+
+// middleware
+function extractToken(req, res, next) {
+    const bearerHeader = req.headers['authorization']; // Bearer token
+    if(bearerHeader !== undefined){
+        const bearer = bearerHeader.split(' ');// ['Bearer', 'token']
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next()
+    }
+    else{
+        res.sendStatus(403)
+    }
+}
 
 app.listen(port, function(err){
     if(err){
